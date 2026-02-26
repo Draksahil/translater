3968f4fea122cd8cf3a3f2c8e05c06152f819057
SK MD SAHIL AKTAR <97446538+Draksahil@users.noreply.github.com>
2026-02-26 23:52:23 +0530

    Add files via upload

---
+++ countrys.js
@@ -0,0 +1,10 @@
+const countries = {
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "ko-KR": "Korean"
+  // ... all others
+};
